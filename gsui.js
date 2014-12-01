9570fa3719e744f63d48a54077a341e57f2fd387
Alex Goodger <Alex@Alexs-MacBook-Pro-6.local>
2014-12-01 11:36:11 +0000

    Added the ability to pass a repeat: <time> option via the options parameter. Improved handling of some transitions.

--- gsui.js
+++ gsui.js
@@ -1,47 +1,38 @@
 /*global $el:true */
 /*global TimelineLite */
 /*global console */
-
+/*jshint -W079 */
+/*jslint latedef:false*/
+var $ = jQuery.noConflict();
 // Timelines for animations
 var animate;
 // Distances
 var smallPx=50, bigPx= 500, nudge=10;
 // Timing variables & defaults
 var 
-duration, delay, easing, stagger, repeat, calc;
+duration, delay, easing, stagger, calc, cb, count=0;
 var 
-defaultDuration = 0.5, defaultDelay = 0, defaultEasing = "Linear.easeNone", defaultStagger = 0.3, defaultRepeat = 0;
+defaultDuration = 0.5, defaultDelay = 0, defaultEasing = "Linear.easeNone", defaultStagger = 0.3;
 // Element/s that is/are being animated
 $el="";
-function customParams(options) {
-	duration = defaultDuration; 
-	delay = defaultDelay;
-	easing = defaultEasing;
-	stagger = defaultStagger;
-	repeat = defaultRepeat;
-	if(typeof(options.delay) !== "undefined") { delay = options.delay; console.log(delay); }
-	if(typeof(options.duration) !== "undefined") { duration = options.duration; }
-	if(typeof(options.easing) !== "undefined") { easing = options.easing; }
-	if(typeof(options.stagger) !== "undefined") { stagger = options.stagger; }
-	if(typeof(options.repeat) !== "undefined") { repeat = options.repeat; }
-}
 function resetParams() {
 	duration = defaultDuration; 
 	delay = defaultDelay;
 	easing = defaultEasing;
 	stagger = defaultStagger;
-	repeat = defaultRepeat;
 }
-function animationComplete($el, callback) {
+function animationComplete($el) {
 	$el.removeClass('gsui-active');
 	$el.css('transform','none');
-	callback();
+	if(typeof(cb)!=="undefined") {
+		cb();
+	}
 }
-function checkProgress($el, element, callback) {
+function checkProgress($el, element) {
 	calc = (duration * element.length) * 1000 + (delay*1000);
 	console.log(calc);
 	setTimeout(function() {
-		animationComplete($el, callback);
+		animationComplete($el);
 	}, calc+50);
 }
 var gsui = function(method, element, options, callback) {
@@ -57,19 +48,19 @@ var gsui = function(method, element, options, callback) {
 	}
 	else {
 		$el.addClass('gsui-active');
-		console.log(arguments[2])
-		// Style reset to prevent unwanted styles persisting between transitions
-		method = (typeof arguments[0] !== "function")? arguments[0] : {},
-		element = (typeof arguments[0] !== "function")? arguments[1] : {},
-		options = (typeof arguments[0] !== "function")? arguments[2] : {},
-		callback = (typeof arguments[0] !== "function")? arguments[3] : arguments[0];
+		cb = callback;
 		animate = new TimelineLite();
 		resetParams();
 		if(typeof(options) !== "undefined")
 		{
-			customParams(options);
-			
+			if(count<options.repeat-1) {
+				gsui(method, element, options, callback);
+				count++;
+			}
+				
+				customParams(options);
 		}
+		console.log(duration);
 		switch (method) {
 		/**************************
 		UNIFIED - for arrays of 
@@ -78,52 +69,52 @@ var gsui = function(method, element, options, callback) {
 		**************************/
 		case 'slideInLeft':
 		animate.fromTo( 
-			element, duration, {delay: delay, left: "-=" + smallPx + "px", opacity: 0}, {delay: delay, left: "0px", opacity: 1, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, left: "-=" + smallPx + "px", opacity: 0}, {delay: delay, left: "0px", opacity: 1, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'slideInLeftBig':
 		animate.from( 
-			element, duration, {delay: delay, left: "-=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, left: "-=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'slideOutLeft':
 		animate.to( 
-			element, duration, {delay: delay, left: "-=" + smallPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, left: "-=" + smallPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'slideOutLeftBig':
 		animate.to( 
-			element, duration, {delay: delay, left: "-=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, left: "-=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'slideInRight':
 		animate.from( 
-			element, duration, {delay: delay, left: "+=" + smallPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, left: "+=" + smallPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'slideInRightBig':
 		animate.from( 
-			element, duration, {delay: delay, left: "+=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, left: "+=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'slideOutRight':
 		animate.to( 
-			element, duration, {delay: delay, left: "+=" + smallPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, left: "+=" + smallPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'slideOutRightBig':
 		animate.to( 
-			element, duration, {delay: delay, left: "+=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, left: "+=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'slideInUp':
 		animate.from( 
-			element, duration, {delay: delay, top: "+=" + smallPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, top: "+=" + smallPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'slideInUpBig':
 		animate.from( 
-			element, duration, {delay: delay, top: "+=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, top: "+=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'slideOutUp':
@@ -133,37 +124,37 @@ var gsui = function(method, element, options, callback) {
 		break;
 		case 'slideOutUpBig':		
 		animate.to( 
-			element, duration, {delay: delay, top: "-=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, top: "-=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'slideInDown':		
 		animate.from( 
-			element, duration, {delay: delay, top: "-=" + smallPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, top: "-=" + smallPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'slideInDownBig':
 		animate.from( 
-			element, duration, {delay: delay, top: "-=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, top: "-=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'slideOutDown':
 		animate.to( 
-			element, duration, {delay: delay, top: "+=" + smallPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, top: "+=" + smallPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'slideOutDownBig':
 		animate.to( 
-			element, duration, {delay: delay, top: "+=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, top: "+=" + bigPx + "px", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'zoomIn':
 		animate.fromTo( 
-			element, duration, {delay: delay, scale: "0", opacity: 0, ease:easing}, {scale: "1", opacity: 1, onComplete: function() { animationComplete($el, callback);}}, stagger
+			element, duration, {scale: "0", opacity: 0}, {scale: "1", opacity: 1,delay: delay, ease:easing, onComplete: function() { animationComplete($el);}}, stagger
 			);
 		break;
 		case 'zoomOut':
 		animate.to( 
-			element, duration, {delay: delay, scale: "0%", opacity: 0, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration, {delay: delay, scale: "0%", opacity: 0, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 		break;
 		case 'tada':
@@ -176,7 +167,7 @@ var gsui = function(method, element, options, callback) {
 			).to( 
 			element, duration/6, {rotation: "-10%", scale: 1.1, ease:easing}
 			).to( 
-			element, duration/6, {rotation: "0%", scale: 1, ease:easing, onComplete: function() { animationComplete($el, callback);}}
+			element, duration/6, {rotation: "0%", scale: 1, ease:easing, onComplete: function() { animationComplete($el);}}
 			);
 			break;
 			case 'shake':
@@ -184,7 +175,7 @@ var gsui = function(method, element, options, callback) {
 			animate.to( element, duration/2, {x: (nudge/2), ease:easing})
 			.to(element, duration/2, {x: "-=" + nudge, ease:easing, repeat:1})
 			.to(element, duration/2, {x: "+=" + nudge, ease:easing, repeat:1})
-			.to( element, duration/2, {x: "0", ease:easing, onComplete: function() { animationComplete($el, callback);}});
+			.to( element, duration/2, {x: "0", ease:easing, onComplete: function() { animationComplete($el);}});
 			break;
 		/**************************
 		STAGGERED - for arrays of 
@@ -195,112 +186,112 @@ var gsui = function(method, element, options, callback) {
 		animate.staggerFrom( 
 			element, duration, {delay: delay, left: "-=" + smallPx + "px", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideInLeftBig':
 		animate.staggerFromTo( 
 			element, duration, {left: "-=" + bigPx + "px", opacity: 0}, {delay: delay, left: "0px", opacity: 1, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideOutLeft':
 		animate.staggerTo( 
 			element, duration, {delay: delay, left: "-=" + smallPx + "px", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideOutLeftBig':
 		animate.staggerTo( 
 			element, duration, {delay: delay, left: "-" + bigPx + "px", opacity: 0, ease:easing}, {left: "0", opacity: 1}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideInRight':
 		animate.staggerFrom( 
 			element, duration, {delay: delay, left: "+=" + smallPx + "px", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideInRightBig':
 		animate.staggerFrom( 
 			element, duration, {delay: delay, left: "+=" + bigPx + "px", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideOutRight':
 		animate.staggerTo( 
 			element, duration, {delay: delay, left: "+=" + smallPx + "px", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideOutRightBig':
 		animate.staggerTo( 
 			element, duration, {delay: delay, left: "+=" + bigPx + "px", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideInUp':
 		animate.staggerFrom( 
 			element, duration, {delay: delay, top: "+=" + smallPx + "px", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideInUpBig':
 		animate.staggerFrom( 
 			element, duration, {delay: delay, top: "+=" + bigPx + "px", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideOutUp':
 		animate.staggerTo( 
 			element, duration, {delay: delay, top: "-=" + smallPx + "px", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideOutUpBig':		
 		animate.staggerTo( 
 			element, duration, {delay: delay, top: "-=" + bigPx + "px", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideInDown':		
 		animate.staggerFrom( 
 			element, duration, {delay: delay, top: "-=" + smallPx + "px", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideInDownBig':
 		animate.staggerFrom( 
 			element, duration, {delay: delay, top: "-=" + bigPx + "px", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideOutDown':
 		animate.staggerTo( 
 			element, duration, {delay: delay, top: "+=" + smallPx + "px", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.slideOutDownBig':
 		animate.staggerTo( 
 			element, duration, {delay: delay, top: "+=" + bigPx + "px", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.zoomIn':
+		console.log(delay);
 		animate.staggerFromTo( 
 			element, duration, {delay: delay, scale: "0", opacity: 0, ease:easing}, {scale: "1", opacity: 1}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.zoomOut':
 		animate.staggerTo( 
 			element, duration, {delay: delay, scale: "0%", opacity: 0, ease:easing}, stagger
 			);
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.tada':
-		// duration=duration*5;
 		element.each(function(index,element){
 			// This effectively allows the user to set a 
 			// stagger time for sequences with multiple tweens
@@ -318,24 +309,34 @@ var gsui = function(method, element, options, callback) {
 					element, duration/5, {rotation: "0%", ease:easing});
 				}, (index*stagger*1000)+delay*1000);
 		});
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		case 'stagger.shake':
 		element.each(function(index,element){
 			setTimeout(function() {
-				animate.to( element, duration/6, {x: (nudge/2), ease:easing})
-				.to(element, duration/6, {x: "-=" + nudge, ease:easing})
-				.to(element, duration/6, {x: "+=" + nudge, ease:easing})
-				.to(element, duration/6, {x: "-=" + nudge, ease:easing})
-				.to(element, duration/6, {x: "+=" + nudge, ease:easing})
-				.to( element, duration/6, {x: "0", ease:easing});
+				animate.to( element, duration/6, {left: (nudge/2), opacity: 1, ease:easing})
+				.to(element, duration/6, {left: "-=" + nudge, ease:easing})
+				.to(element, duration/6, {left: "+=" + nudge, ease:easing})
+				.to(element, duration/6, {left: "-=" + nudge, ease:easing})
+				.to(element, duration/6, {left: "+=" + nudge, ease:easing})
+				.to( element, duration/6, {left: "0", ease:easing});
 			}, (index*stagger*1000)+delay*1000);
 		});
-		checkProgress($el, element, callback);
+		checkProgress($el, element);
 		break;
 		default:
 		console.log('The command ' + method + ' is not valid. Please refer to the documentation for accepted methods.');
 		break;
 	}
+}
+function customParams(options) {
+	duration = defaultDuration; 
+	delay = defaultDelay;
+	easing = defaultEasing;
+	stagger = defaultStagger;
+	if(typeof(options.delay) !== "undefined") { delay = options.delay; console.log(delay); }
+	if(typeof(options.duration) !== "undefined") { duration = options.duration; }
+	if(typeof(options.easing) !== "undefined") { easing = options.easing; }
+	if(typeof(options.stagger) !== "undefined") { stagger = options.stagger; }
 }
 };
